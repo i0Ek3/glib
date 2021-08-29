324eec30da63d5124c6da11cd23785b79ceb062d
i0Ek3 <kno30826@gmail.com>
2021-08-29 10:00:55 +0800

    feat: Add lock and generic support for set

--- set/set.go
+++ set/set.go
@@ -1,27 +1,40 @@
 package set
 
+import (
+    "sync"
+)
+
+// T/K for generic use
+type (
+    T interface{}
+    K struct{}
+)
+
 // Set defines a set struct which made of map
 type Set struct {
-    set map[interface{}]struct{}
+    set  map[T]K
+    lock sync.RWMutex
 }
 
 // NewSet creates a set and add vals to this set
-func NewSet(vals ...interface{}) *Set {
+func NewSet(vals ...T) *Set {
     s := &Set{}
-    s.set = make(map[interface{}]struct{})
+    s.set = make(map[T]K)
     s.Add(vals...)
     return s
 }
 
 // Add adds given values into set
-func (s *Set) Add(vals ...interface{}) {
+func (s *Set) Add(vals ...T) {
     for _, v := range vals {
-        s.set[v] = struct{}{}
+        s.lock.Lock()
+        s.set[v] = K{}
+        s.lock.Unlock()
     }
 }
 
 // Remove removes the given value from set
-func (s *Set) Remove(vals interface{}) {
+func (s *Set) Remove(vals T) {
     delete(s.set, vals)
 }
 
@@ -31,7 +44,7 @@ func (s *Set) Clear() {
 }
 
 // Query checks if the given value exist in set
-func (s *Set) Query(vals interface{}) bool {
+func (s *Set) Query(vals T) bool {
     _, ok := s.set[vals]
     return ok
 }
