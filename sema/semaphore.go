76a8d398a7ddddba6a2d5a3e33d2fddab5b35b9c
i0Ek3 <kno30826@gmail.com>
2022-05-16 22:55:28 +0800

    feat: add semaphore for Go

---
+++ sema/semaphore.go
@@ -0,0 +1,40 @@
+package sema
+
+import "sync"
+
+// Semaphore indicates a semaphore
+type Semaphore struct {
+    sync.Locker
+    ch chan struct{}
+}
+
+// NewSemaphore returns a sync.Locker interface
+func (s *Semaphore) NewSemaphore(capa int) sync.Locker {
+    if capa <= 0 {
+        capa = 1
+    }
+    return &Semaphore{
+        // create a buffered channel which capacity is capa
+        ch: make(ch struct{}, capa),
+    }
+}
+
+// Lock acquires a resource
+func (s *Semaphore) Lock() {
+    s.ch <- struct{}{}
+}
+
+// Unlock releases a resource
+func (s *Semaphore) Unlock() {
+    <-s.ch
+}
+
+// TryLock trys to acquire a resource
+func (s *Semaphore) TryLock() bool {
+    select {
+    case s.ch <- struct{}{}:
+        return true
+    default:
+    }
+    return false
+}
