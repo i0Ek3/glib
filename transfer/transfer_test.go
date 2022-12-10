12548eeb0f8385a727aefd53a66a44dd214afa38
i0Ek3 <kno30826@gmail.com>
2022-12-10 19:06:19 +0800

    test: Add performance test

--- transfer/transfer_test.go
+++ transfer/transfer_test.go
@@ -1,25 +1,71 @@
 package transfer
 
 import (
-	"github.com/i0Ek3/asrt"
 	"testing"
+
+	"github.com/i0Ek3/asrt"
 )
 
 func TestS2B(t *testing.T) {
-    oristr := "orignal string"
-    got := S2B(oristr)
-    want := []byte(oristr)
+	oristr := "orignal string"
+	got := S2B(oristr)
+	want := []byte(oristr)
 
-    asrt.Equal(got, want)
+	asrt.Equal(got, want)
+}
+
+func BenchmarkS2B(b *testing.B) {
+	oristr := "orignal string"
+	for i := 0; i < b.N; i++ {
+		_ = S2B(oristr)
+	}
 }
 
 func TestB2S(t *testing.T) {
-    for i := 'A'; i <= 'Z'; i++ {
-        oribyt := []byte{byte(i)}
-        got := B2S(oribyt)
-        want := string(oribyt)
-        asrt.Equal(got, want)
-    } 
+	for i := 'A'; i <= 'Z'; i++ {
+		oribyt := []byte{byte(i)}
+		got := B2S(oribyt)
+		want := string(oribyt)
+		asrt.Equal(got, want)
+	}
+}
+
+func BenchmarkB2S(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 'A'; j <= 'Z'; j++ {
+			oribyt := []byte{byte(j)}
+			_ = B2S(oribyt)
+		}
+	}
+}
+
+func TestB2R(t *testing.T) {
+	for i := byte('A'); i <= ('Z'); i++ {
+		_ = B2R(i)
+	}
+}
+
+func BenchmarkB2R(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := byte('A'); j <= ('Z'); j++ {
+			_ = B2R(j)
+		}
+	}
+}
+
+func TestR2B(t *testing.T) {
+	for i := 'A'; i <= 'Z'; i++ {
+		_ = R2B(i)
+	}
+}
+
+func BenchmarkR2B(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 'A'; j <= 'Z'; j++ {
+			_ = R2B(j)
+		}
+
+	}
 }
 
 func TestReverseWithInterval(t *testing.T) {
@@ -30,6 +76,13 @@ func TestReverseWithInterval(t *testing.T) {
 	asrt.Equal(got, want)
 }
 
+func BenchmarkReverseWithInterval(b *testing.B) {
+	s := "hello"
+	for i := 0; i < b.N; i++ {
+		_ = ReverseWithInterval([]byte(s), 0, 4)
+	}
+}
+
 func TestReverse(t *testing.T) {
 	res := []int{1, 2, 3, 4, 5}
 	got := Reverse(res)
@@ -38,11 +91,26 @@ func TestReverse(t *testing.T) {
 	asrt.Equal(got, want)
 }
 
+func BenchmarkReverse(b *testing.B) {
+	res := []int{1, 2, 3, 4, 5}
+	for i := 0; i < b.N; i++ {
+		_ = Reverse(res)
+	}
+}
+
 func TestLower(t *testing.T) {
 	for i := byte('A'); i <= byte('Z'); i++ {
 		got := Lower(i)
 		if got != i+32 {
 			t.Errorf("got %v != want %v", got, i+32)
+		}
+	}
+}
+
+func BenchmarkLower(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := byte('A'); j <= byte('Z'); j++ {
+			_ = Lower(j)
 		}
 	}
 }
